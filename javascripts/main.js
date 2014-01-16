a898497022c2727e204aa7d98676532a3bd9cc6e
Vaibhav Bhandari <vaibhavb@gmail.com>
2014-01-16 10:41:06 +0000

    Updating to 4bd46aaaa602cce517db61084829f2fe34270190.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,10 @@
 console.log('This would be the main JS file.');
 
 $(document).ready(function() {
-	var img = ($('.circular-hidden').text());
+	var img = ($('.profile-circular-hidden').text());
 	console.log("Hidden Image: " + img);
 	if ( img != '/images/politicians/') {
-		$('.circular').css('background', "url('"+ img +"')");
-		$('.circular').css('background-size', "250px 250px");
+		$('.profile-circular').css('background', "url('"+ img +"')");
+		$('.profile-circular').css('background-size', "160px 160px");
 	}
 });
-
