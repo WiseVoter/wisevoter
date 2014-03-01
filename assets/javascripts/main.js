d05525541aa4acfffbc3b94e3e38b6402bbd0b14
root <root@directinabox.(none)>
2014-02-28 19:25:27 -0500

    Node bot commit

---
+++ assets/javascripts/main.js
@@ -0,0 +1,10 @@
+var mainsite = "www.wisevoter.org"
+  , editsite = "edit.wisevoter.org";
+
+function setEditorLink(){
+  var editorLink = document.getElementById("editorLink")
+  if (document.location.hostname === mainsite)
+    editorLink.href = document.location.replace(mainsite, editsite) + editorLink.href;
+}
+
+window.onload = setEditorLink;
