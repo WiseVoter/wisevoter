cac3b5dd1f6a7536fa4eee6c9f5bf3660a6aedc4
vaibhav bhandari <vaibhavb@gmail.com>
2014-03-03 04:35:44 +0000

    adding new ui and search

--- site/assets/javascripts/main.js
+++ site/assets/javascripts/main.js
@@ -12,3 +12,34 @@ function setEditorLink(){
 window.onload = function(){
   setEditorLink();
 }
+
+// instantiate the bloodhound suggestion engine
+var search = new Bloodhound({   
+  datumTokenizer: function(d) {
+      return Bloodhound.tokenizers.whitespace(d.text); 
+    },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: '../../search.json'
+});
+ 
+// initialize the bloodhound suggestion engine
+search.initialize();
+
+// instantiate the typeahead UI
+$('.search-input').typeahead(null, {
+  displayKey: 'href',
+  source: search.ttAdapter(),
+  templates: {
+    suggestion: Handlebars.compile([
+        '<div class="search-elem">',
+        '<p class="search-text">{{text}}</p>',
+        '<p class="search-category">{{category}}</p>',
+        '<p class="search-href">{{href}}</p>',
+        '</div>'
+    ].join(''))
+  }
+});
+
+function searchSubmit(){
+  console.log("redirecting..")
+}
