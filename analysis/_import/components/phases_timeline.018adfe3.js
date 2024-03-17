506eb35f41dd4806391989ed696339583032ef32
Vaibhav Bhandari <vaibhavb@gmail.com>
2024-03-17 12:42:09 -0700

    new analysis with election phases

---
+++ analysis/_import/components/phases_timeline.018adfe3.js
@@ -0,0 +1,16 @@
+import * as Plot from "../../_npm/@observablehq/plot@0.6.14/+esm.js";
+
+export function phases_timeline(events, {width, height} = {}) {
+  return Plot.plot({
+    width,
+    height,
+    marginTop: 30,
+    x: {nice: true, label: null, tickFormat: ""},
+    y: {axis: null},
+    marks: [
+      Plot.ruleX(events, {x: d => new Date(d.date_of_poll), y: "no_of_constituencies", markerEnd: "dot", strokeWidth: 2.5}),
+      Plot.ruleY([0]),
+      Plot.text(events, {x: d => new Date(d.date_of_poll), y: "no_of_constituencies", text: "date_of_poll", lineAnchor: "bottom", dy: -10, lineWidth: 10, fontSize: 12})
+    ]
+  });
+}
--- analysis/_import/components/timeline.4e6d0a6e.js
+++ analysis/_import/components/timeline.4e6d0a6e.js
@@ -1,4 +1,4 @@
-import * as Plot from "../../_npm/@observablehq/plot@0.6.13/+esm.js";
+import * as Plot from "../../_npm/@observablehq/plot@0.6.14/+esm.js";
 
 export function timeline(events, {width, height} = {}) {
   return Plot.plot({
