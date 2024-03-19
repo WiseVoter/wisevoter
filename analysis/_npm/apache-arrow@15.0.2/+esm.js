ed0cfab84902a7dfca3940136b285d83fca9f6c7
Vaibhav Bhandari <vaibhavb@gmail.com>
2024-03-18 18:37:22 -0700

    pages with interactivity

--- analysis/_npm/apache-arrow@15.0.2/+esm.js
+++ analysis/_npm/apache-arrow@15.0.2/+esm.js
@@ -1,6 +1,6 @@
 /**
  * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
- * Original file: /npm/apache-arrow@15.0.1/Arrow.dom.mjs
+ * Original file: /npm/apache-arrow@15.0.2/Arrow.dom.mjs
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
  */
