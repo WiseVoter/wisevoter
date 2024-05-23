c42d0ba9aea47ae48b940b89da8b62dec158136e
Vaibhav Bhandari <vaibhavb@gmail.com>
2024-05-23 02:15:47 -0700

    adding criminals

--- analysis/_import/components/timeline.4349c23f.js
+++ analysis/_import/components/timeline.4349c23f.js
@@ -1,4 +1,4 @@
-import * as Plot from "../../_npm/@observablehq/plot@0.6.14/+esm.js";
+import * as Plot from "../../_npm/@observablehq/plot@0.6.14/_esm.js";
 
 export function phases_timeline(events, {width, height} = {}) {
   return Plot.plot({
